684ab4ab1b631bd65b435dce605e5d97590e9a23
WhiteMaks <whitemaks.working@gmail.com>
2024-03-31 14:02:20 +0300

    support webgl context into gc factory

--- GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
+++ GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
@@ -4,7 +4,8 @@
 #include "Core/GraphicsEngine/OpenGL/OpenGLContext.h"
 #include "Core/Platform/Windows/WindowsWindow.h"
 #elif GAME_ENGINE_PLATFORM_BROWSER
-
+#include "Core/Platform/Browser/BrowserWindow.h"
+#include "Core/GraphicsEngine/WebGL/WebGLContext.h"
 #endif
 
 namespace GraphicsEngine
@@ -14,8 +15,10 @@ namespace GraphicsEngine
 	{
 #ifdef GAME_ENGINE_PLATFORM_WINDOWS
 		return new OpenGLContext(Platform::WindowsWindow::window);
+#elif GAME_ENGINE_PLATFORM_BROWSER
+		return new WebGLContext(Platform::BrowserWindow::window);
 #else
-#error Graphics context only supports Windows!
+#error Graphics context only supports Windows and Browser!
 #endif
 	}
 

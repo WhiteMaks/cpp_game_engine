ba57cf1f8fb375dfd890a0fcf9cc06bfa87fd254
WhiteMaks <whitemaks.working@gmail.com>
2024-04-03 20:34:48 +0300

    Update GraphicsContextFactory.cpp

--- GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
+++ GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
@@ -3,6 +3,7 @@
 #ifdef GAME_ENGINE_PLATFORM_WINDOWS
 #include "Core/GraphicsEngine/OpenGL/OpenGLContext.h"
 #include "Core/GraphicsEngine/DirectX/DirectX11Context.h"
+#include "Core/GraphicsEngine/DirectX/DirectX12Context.h"
 #include "Core/Platform/Windows/WindowsWindow.h"
 #elif GAME_ENGINE_PLATFORM_BROWSER
 #include "Core/Platform/Browser/BrowserWindow.h"
@@ -16,7 +17,8 @@ namespace GraphicsEngine
 	{
 #ifdef GAME_ENGINE_PLATFORM_WINDOWS
 		//return new OpenGLContext(Platform::WindowsWindow::window);
-		return new DirectX11Context(Platform::WindowsWindow::window);
+		//return new DirectX11Context(Platform::WindowsWindow::window);
+		return new DirectX12Context(Platform::WindowsWindow::window);
 #elif GAME_ENGINE_PLATFORM_BROWSER
 		return new WebGLContext(Platform::BrowserWindow::window);
 #else

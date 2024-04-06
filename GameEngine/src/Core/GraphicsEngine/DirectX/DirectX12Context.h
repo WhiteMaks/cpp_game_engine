994210315b36ea5b3845e76b0459cf5993f634f0
WhiteMaks <whitemaks.working@gmail.com>
2024-04-06 12:07:11 +0300

    init d3d12

--- GameEngine/src/Core/GraphicsEngine/DirectX/DirectX12Context.h
+++ GameEngine/src/Core/GraphicsEngine/DirectX/DirectX12Context.h
@@ -4,10 +4,12 @@
 #include <dxgi1_6.h>
 #include <wrl.h>
 #include <Windows.h>
+#include <comdef.h>
 
 #include "Core/GraphicsEngine/Renderer/GraphicsContext.h"
 #include "Tools/Log.h"
 #include "ExitCodes.h"
+#include "d3dx12.h"
 
 namespace GraphicsEngine
 {
@@ -15,10 +17,28 @@ namespace GraphicsEngine
 	class DirectX12Context : public GraphicsContext
 	{
 	private:
+		static const UINT bufferCount = 2;
+
 		HWND window;
-		Microsoft::WRL::ComPtr<IDXGIFactory6> factory;
+
+		Microsoft::WRL::ComPtr<IDXGIFactory7> factory;
 		Microsoft::WRL::ComPtr<IDXGIAdapter4> adapter;
 		Microsoft::WRL::ComPtr<ID3D12Device10> device;
+		Microsoft::WRL::ComPtr<IDXGISwapChain4> swapChain;
+		Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvDescriptorHeap;
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList7> commandList;
+		
+		Microsoft::WRL::ComPtr<ID3D12Fence1> fence[bufferCount];
+		Microsoft::WRL::ComPtr<ID3D12Resource2> renderTargetView[bufferCount];
+		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator[bufferCount];
+
+		HANDLE fenceEvent;
+
+		UINT currentBuffer;
+		UINT rtvHeapIncrement;
+
+		UINT64 fenceValue[bufferCount];
 
 	public:
 		DirectX12Context(HWND window) noexcept;
@@ -27,6 +47,18 @@ namespace GraphicsEngine
 		void Init() noexcept override;
 		void SwapBuffers() noexcept override;
 		void Destroy() noexcept override;
+
+	private:
+		void InitDXGIFactory() noexcept;
+		void InitDXGIAdapter() noexcept;
+		void InitD3D12Device() noexcept;
+		void InitD3D12CommandQueue() noexcept;
+		void InitD3D12Fence() noexcept;
+		void InitD3D12CommandAllocator() noexcept;
+		void InitD3D12CommandList() noexcept;
+		void InitDXGISwapChain() noexcept;
+		void InitD3D12RenderTargetView() noexcept;
+		void InitD3D12RTVDescriptorHeap() noexcept;
 	};
 
 }

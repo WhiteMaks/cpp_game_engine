22912d411873311d9dfb4fee71dffafb3fa02866
WhiteMaks <whitemaks.working@gmail.com>
2024-04-19 12:48:41 +0300

    add webgl renderer abstraction

---
+++ GameEngine/src/Core/GraphicsEngine/WebGL/WebGLRenderer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include "Core/GraphicsEngine/Renderer/RendererAPI.h"
+#include "Tools/Log.h"
+#include "ExitCodes.h"
+
+namespace GraphicsEngine
+{
+
+	class WebGLRenderer : public RendererAPI
+	{
+	public:
+		void Init() noexcept override;
+		void Destroy() noexcept override;
+
+		void ClearColorBuffer() noexcept override;
+		void SetClearColor(const glm::vec4& color) noexcept override;
+		void DrawTriangles(std::shared_ptr<VertexArrayBuffer> buffer) noexcept override;
+	};
+
+}

ca5b830c5df887aecc36a733446e4e2c6d7e2fd2
WhiteMaks <whitemaks.working@gmail.com>
2024-08-08 15:13:28 +0300

    comment directx lines

--- GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
+++ GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
@@ -4,8 +4,8 @@
 #include "Core/Platform/Browser/BrowserWindow.h"
 #include "Core/GraphicsEngine/WebGL/WebGLContext.h"
 #include "Core/GraphicsEngine/OpenGL/OpenGLContext.h"
-#include "Core/GraphicsEngine/DirectX/DirectX11Context.h"
-#include "Core/GraphicsEngine/DirectX/DirectX12Context.h"
+//#include "Core/GraphicsEngine/DirectX/DirectX11Context.h"
+//#include "Core/GraphicsEngine/DirectX/DirectX12Context.h"
 #include "Core/GraphicsEngine/GraphicsEngine.h"
 #include "Core/Platform/Windows/WindowsWindow.h"
 #include "Core/Platform/Glfw/GlfwWindow.h"
@@ -24,8 +24,8 @@ namespace GraphicsEngine
 		{
 		case GraphicsAPI::WebGL: return new WebGLContext(Platform::BrowserWindow::window);
 		case GraphicsAPI::OpenGL: return new OpenGLContext(Platform::GlfwWindow::window);
-		case GraphicsAPI::DirectX_11: return new DirectX11Context(Platform::WindowsWindow::window);
-		case GraphicsAPI::DirectX_12: return new DirectX12Context(Platform::WindowsWindow::window);
+		//case GraphicsAPI::DirectX_11: return new DirectX11Context(Platform::WindowsWindow::window);
+		//case GraphicsAPI::DirectX_12: return new DirectX12Context(Platform::WindowsWindow::window);
 		}
 #elif GAME_ENGINE_PLATFORM_BROWSER
 		return new WebGLContext(Platform::BrowserWindow::window);

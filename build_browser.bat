d760b7751395f4210846bacd462a32240bfeb131
WhiteMaks <whitemaks.working@gmail.com>
2024-08-08 16:21:47 +0300

    fix

--- build_browser.bat
+++
@@ -1,6 +0,0 @@
-set GAME_ENGINE_PLATFORM="BROWSER"
-mkdir build
-cd build
-emsdk activate
-emcmake cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --config Release -- -j8
--- build_browser.ps1
+++ build_browser.ps1
@@ -1,7 +1,8 @@
 $env:GAME_ENGINE_PLATFORM="BROWSER"
 mkdir build
 cd build
-emsdk activate
+$env:path
+../emsdk/emsdk.exe activate
 emcmake cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release -- -j8
 pause

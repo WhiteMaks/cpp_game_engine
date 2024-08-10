3cc753961eb23c92411b7d1d6799b1022aa201d8
WhiteMaks <whitemaks.working@gmail.com>
2024-08-10 14:16:21 +0300

    Update server.py

--- Sandbox/server.py
+++ Sandbox/server.py
@@ -24,6 +24,6 @@ class Handler(SimpleHTTPRequestHandler):
 
 if __name__ == '__main__':
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
-    with socketserver.TCPServer(("0.0.0.0", port), Handler) as httpd:
+    with socketserver.TCPServer(("localhost", port), Handler) as httpd:
         print("Serving on port", port)
         httpd.serve_forever()

e56b0bf6885b300195e7a00f8df56984b782b83e
WhiteMaks <whitemaks.working@gmail.com>
2024-08-22 18:07:41 +0300

    refactoring

--- GameEngine/src/Core/GraphicsEngine/OpenGL/OpenGLFrameBuffer.cpp
+++ GameEngine/src/Core/GraphicsEngine/OpenGL/OpenGLFrameBuffer.cpp
@@ -58,21 +58,17 @@ namespace GraphicsEngine
 	{
 		glCreateTextures(GL_TEXTURE_2D, 1, &colorAttachmentId);
 		colorAttachment = std::shared_ptr<OpenGLTexture>(new OpenGLTexture(colorAttachmentId));
-		colorAttachment->Bind();
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, data.Width, data.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glTextureStorage2D(colorAttachmentId, 1, GL_RGBA8, data.Width, data.Height);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachmentId, 0);
-		colorAttachment->Unbind();
 	}
 
 	void OpenGLFrameBuffer::InitDepthAttachment() noexcept
 	{
 		glCreateTextures(GL_TEXTURE_2D, 1, &depthAttachmentId);
-		glBindTexture(GL_TEXTURE_2D, depthAttachmentId);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, data.Width, data.Height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, nullptr);
+		glTextureStorage2D(depthAttachmentId, 1, GL_DEPTH24_STENCIL8, data.Width, data.Height);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, depthAttachmentId, 0);
-		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
 	void OpenGLFrameBuffer::CheckBufferStatus() noexcept

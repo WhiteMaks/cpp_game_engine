4a7349f9802682b7601dc1a0a46d686ae64b68a2
WhiteMaks <whitemaks.working@gmail.com>
2024-11-30 23:18:41 +0300

    fix warnings

--- GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
+++ GameEngine/src/Core/GraphicsEngine/Renderer/GraphicsContextFactory.cpp
@@ -1,14 +1,16 @@
 #include "Core/GraphicsEngine/Renderer/GraphicsContextFactory.h"
 
 #ifdef GAME_ENGINE_PLATFORM_WINDOWS
+#include "Core/GraphicsEngine/GraphicsEngine.h"
+
 #include "Core/Platform/Browser/BrowserWindow.h"
 #include "Core/GraphicsEngine/WebGL/WebGLContext.h"
+
+#include "Core/Platform/Glfw/GlfwWindow.h"
 #include "Core/GraphicsEngine/OpenGL/OpenGLContext.h"
 //#include "Core/GraphicsEngine/DirectX/DirectX11Context.h"
 //#include "Core/GraphicsEngine/DirectX/DirectX12Context.h"
-#include "Core/GraphicsEngine/GraphicsEngine.h"
-#include "Core/Platform/Windows/WindowsWindow.h"
-#include "Core/Platform/Glfw/GlfwWindow.h"
+//#include "Core/Platform/Windows/WindowsWindow.h"
 #elif GAME_ENGINE_PLATFORM_BROWSER
 #include "Core/Platform/Browser/BrowserWindow.h"
 #include "Core/GraphicsEngine/WebGL/WebGLContext.h"
